59221afde0d8e18e62e99f51ca1e0f8f5d99b9c2
ChaoCmb2016 <chao_cmb@yeah.net>
2018-10-10 19:33:07 +0800

    2018-10-10

--- src/components/Searcher/Searcher.js
+++ src/components/Searcher/Searcher.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Searcher.css'
-const Mysearcher=(props)=>{
-  
+const Mysearcher = (props) => {
+
     return (
         <div className="Seacher">
-              <i className="icon-search"></i> 
+            <i className="icon-search"></i>
             <input type="text" onChange={props.myOnchaged}
-            className="input-search" placeholder="关键字搜索"></input>
+                className="input-search" placeholder="关键字搜索"></input>
         </div>
     )
 }
